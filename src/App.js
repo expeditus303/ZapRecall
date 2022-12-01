9309302fd9c37689dfb977cc77c3a70ecc07dbf5
expeditus303 <ricardotonietto@gmail.com>
2022-12-01 14:00:10 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/img/logo.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="screen-container">
+      <div className="logo-container">
+        <img src={logo} alt="" />
+        <h1>ZapRecall</h1>
+      </div>
+      <div className="pergunta-aberta">
+        <p>"JSX é uma sintaxe para escrever HTML dentro do JS"</p>
+        <div className="container-botoes">
+          <button>Não lembrei</button>
+          <button>Quase não lembrei</button>
+          <button>Zap!</button>
+        </div>
+      </div>
+
+      <div className="pergunta-fechada">
+        <p>2</p>
+      </div>
+
+      <div className="pergunta-fechada">
+        <p>3</p>
+      </div>
+
+      <div className="pergunta-fechada">
+        <p>4</p>
+      </div>
+
+      <div className="footer-concluidos"> 10
+      </div>
+    </div >
   );
 }
 
